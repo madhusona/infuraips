10ab607e6e4f47bc298d2d1603b82c3d80a84171
madhusona <madhusona@gmail.com>
2018-10-13 19:21:38 +0530

    ipfs infura

---
+++ src/storehash.js
@@ -0,0 +1,35 @@
+import web3 from './Web3';
+//Your contract address
+const address = '0x84f6ebb4ee64a943f8723089a38b6b019aaab45d';
+//Your contract ABI
+const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"name": "setHash",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHash",
+		"outputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]  
+export default new web3.eth.Contract(abi, address);
